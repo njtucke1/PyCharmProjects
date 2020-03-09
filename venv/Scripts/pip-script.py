5c7e2d1b64794aaf8f8481f242fc02663a8c2f6e
Nicolas Tucker <njtucke1@uncc.edu>
2020-03-09 16:38:24 -0400

    first commit

---
+++ venv/Scripts/pip-script.py
@@ -0,0 +1,12 @@
+#!"C:\Users\Nicolas Tucker\Desktop\UNCC\SPRING 2020\ITSC 3155 Software Engineering\PyCharmProjects\venv\Scripts\python.exe"
+# EASY-INSTALL-ENTRY-SCRIPT: 'pip==19.0.3','console_scripts','pip'
+__requires__ = 'pip==19.0.3'
+import re
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.argv[0] = re.sub(r'(-script\.pyw?|\.exe)?$', '', sys.argv[0])
+    sys.exit(
+        load_entry_point('pip==19.0.3', 'console_scripts', 'pip')()
+    )
